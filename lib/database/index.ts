050044e76f6ba846e8f715f3ce1818892c01f039
YASSIRFX <saberyassir803@gmail.com>
2024-07-30 20:20:08 +0100

    implement stripe

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 
-
 const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
@@ -11,7 +10,7 @@ export const connectToDatabase = async () => {
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'evently',
+    dbName: 'event',
     bufferCommands: false,
   })
 
